ab1a73b53bbd49ec34c10ae530e5ae9d905ec5f9
John Doe <john.doe@example.com>
2026-01-19 21:36:01 +0100

    feat(pagemaster): Add streaming output and low-quality preview optimizations
    - Streaming output: Write documents immediately to disk after assembly
      instead of storing all in memory. Dramatically reduces memory for large merges.
    - Low-quality preview: Render thumbnails at 50% resolution then scale up.
      Much faster initial display for large PDF collections.
    - Renamed to PDF4QT-Opus PageMaster.

--- Pdf4QtPageMaster/main.cpp
+++ Pdf4QtPageMaster/main.cpp
@@ -9,8 +9,8 @@
 // copies of the Software, and to permit persons to whom the Software is
 // furnished to do so, subject to the following conditions:
 //
-// The above copyright notice and this permission notice shall be included in all
-// copies or substantial portions of the Software.
+// The above copyright notice and this permission notice shall be included in
+// all copies or substantial portions of the Software.
 //
 // THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
 // IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
@@ -20,54 +20,55 @@
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
 
+#include "mainwindow.h"
+#include "pdfapplicationtranslator.h"
 #include "pdfconstants.h"
 #include "pdfsecurityhandler.h"
 #include "pdfwidgetutils.h"
-#include "pdfapplicationtranslator.h"
-#include "mainwindow.h"
+
 
 #include <QApplication>
 #include <QCommandLineParser>
 
-int main(int argc, char *argv[])
-{
-    QApplication::setAttribute(Qt::AA_CompressHighFrequencyEvents, true);
-    QApplication application(argc, argv);
+int main(int argc, char *argv[]) {
+  QApplication::setAttribute(Qt::AA_CompressHighFrequencyEvents, true);
+  QApplication application(argc, argv);
 
-    QCoreApplication::setOrganizationName("MelkaJ");
-    QCoreApplication::setApplicationName("PDF4QT PageMaster");
-    QCoreApplication::setApplicationVersion(pdf::PDF_LIBRARY_VERSION);
-    QApplication::setApplicationDisplayName(QApplication::translate("Application", "PDF4QT PageMaster"));
+  QCoreApplication::setOrganizationName("MelkaJ");
+  QCoreApplication::setApplicationName("PDF4QT-Opus PageMaster");
+  QCoreApplication::setApplicationVersion(pdf::PDF_LIBRARY_VERSION);
+  QApplication::setApplicationDisplayName(
+      QApplication::translate("Application", "PDF4QT-Opus PageMaster"));
 
-    QCommandLineOption noDrm("no-drm", "Disable DRM settings of documents.");
-    QCommandLineOption lightGui("theme-light", "Use a light theme for the GUI.");
-    QCommandLineOption darkGui("theme-dark", "Use a dark theme for the GUI.");
+  QCommandLineOption noDrm("no-drm", "Disable DRM settings of documents.");
+  QCommandLineOption lightGui("theme-light", "Use a light theme for the GUI.");
+  QCommandLineOption darkGui("theme-dark", "Use a dark theme for the GUI.");
 
-    QCommandLineParser parser;
-    parser.setApplicationDescription(QCoreApplication::applicationName());
-    parser.addOption(noDrm);
-    parser.addOption(lightGui);
-    parser.addOption(darkGui);
-    parser.addHelpOption();
-    parser.addVersionOption();
-    parser.addPositionalArgument("file", "The PDF file to open.");
-    parser.process(application);
+  QCommandLineParser parser;
+  parser.setApplicationDescription(QCoreApplication::applicationName());
+  parser.addOption(noDrm);
+  parser.addOption(lightGui);
+  parser.addOption(darkGui);
+  parser.addHelpOption();
+  parser.addVersionOption();
+  parser.addPositionalArgument("file", "The PDF file to open.");
+  parser.process(application);
 
-    if (parser.isSet(noDrm))
-    {
-        pdf::PDFSecurityHandler::setNoDRMMode();
-    }
+  if (parser.isSet(noDrm)) {
+    pdf::PDFSecurityHandler::setNoDRMMode();
+  }
 
-    pdf::PDFWidgetUtils::setDarkTheme(parser.isSet(lightGui), parser.isSet(darkGui));
+  pdf::PDFWidgetUtils::setDarkTheme(parser.isSet(lightGui),
+                                    parser.isSet(darkGui));
 
-    pdf::PDFApplicationTranslator translator;
-    translator.installTranslator();
+  pdf::PDFApplicationTranslator translator;
+  translator.installTranslator();
 
-    QIcon appIcon(":/app-icon.svg");
-    QApplication::setWindowIcon(appIcon);
+  QIcon appIcon(":/app-icon.svg");
+  QApplication::setWindowIcon(appIcon);
 
-    pdfpagemaster::MainWindow mainWindow(nullptr);
-    mainWindow.show();
+  pdfpagemaster::MainWindow mainWindow(nullptr);
+  mainWindow.show();
 
-    return application.exec();
+  return application.exec();
 }
